cde849449805d2fbd065128ef36d1e0d4a0fcfb5
abc <abc@gmail.com>
2019-05-09 12:16:39 +0530

    App.java file changed

---
+++ src/main/java/samplemvn/App.java
@@ -0,0 +1,13 @@
+package samplemvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World..........................!" );
+    }
+}
